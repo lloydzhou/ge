ae2f2400d4b81d6ede9731dd4b176be511abad88
lloydzhou <lloydzhou@qq.com>
2026-07-03 18:26:49 +0800

    fix: remove Command export (History snapshot mode)

--- packages/ge-core/src/plugins/index.ts
+++ packages/ge-core/src/plugins/index.ts
@@ -1,7 +1,7 @@
 export * from './plugin';
 export { DragPlugin } from './DragPlugin';
 export { SelectionPlugin } from './SelectionPlugin';
-export { HistoryPlugin, type Command } from './HistoryPlugin';
+export { HistoryPlugin } from './HistoryPlugin';
 export { MinimapPlugin } from './MinimapPlugin';
 export { ScrollerPlugin, type ScrollerOptions } from './ScrollerPlugin';
 export { SnaplinePlugin, type SnaplineOptions } from './SnaplinePlugin';
