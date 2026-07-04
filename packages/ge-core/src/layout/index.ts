6f008cd6ef60626696f7b053b49635ac183a51a4
lloydzhou <lloydzhou@qq.com>
2026-07-04 18:52:12 +0800

    feat: tree layout (LR/TB for mindmap/org chart)

--- packages/ge-core/src/layout/index.ts
+++ packages/ge-core/src/layout/index.ts
@@ -3,3 +3,4 @@ export { gridLayout, type GridOptions } from './grid';
 export { circularLayout, type CircularOptions } from './circular';
 export { forceLayout, type ForceOptions } from './force';
 export { hierarchicalLayout, type HierarchicalOptions } from './hierarchical';
+export { treeLayout, type TreeOptions } from './tree';
